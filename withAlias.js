1adee67ab2505315a59e69bd3ea97cc90a26caf3
yodfz <yodfz@qq.com>
2019-08-25 10:12:45 +0800

    Update withAlias.js

--- withAlias.js
+++ withAlias.js
@@ -2,6 +2,7 @@ const path = require('path')
 module.exports = (nextConfig = {}) => {
     return Object.assign({}, nextConfig, {
         webpack(config, options) {
+            // 相对于当前路径映射
             config.resolve.alias['Components'] = path.join(__dirname, 'components')
             if (typeof nextConfig.webpack === 'function') {
                 return nextConfig.webpack(config, options)
@@ -9,4 +10,4 @@ module.exports = (nextConfig = {}) => {
             return config
         }
     })
-})
+}
